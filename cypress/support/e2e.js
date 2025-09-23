56a9127a906230b8d4eee6059792f7498e99be1f
sehrishnaseer-saad <sehrish.naseer@gmail.com>
2025-09-23 13:22:07 +0500

    download

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,6 +1,7 @@
 // cypress/support/e2e.js
 import './commands';    // ensure this file imports cypress-file-upload
-
+// For file download
+require('cypress-downloadfile/lib/downloadFileCommand');
 
 
 
